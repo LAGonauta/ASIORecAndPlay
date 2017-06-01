20a72bc4c1d7ee650655fd8033886857ade0d7f8
lfsafady <lfsafady@outlook.com>
2017-05-31 22:24:58 -0300

    Initial commit

--- ASIORecAndPlay/MainWindow.xaml.cs
+++ ASIORecAndPlay/MainWindow.xaml.cs
@@ -1,28 +1,147 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Simple program to route audio between ASIO devices
+// Copyright(C) 2017  LAGonauta
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using System;
 using System.Windows;
+using NAudio.Wave;
+using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ASIORecAndPlay
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Based on work by Mark Heath on NAudio ASIO PatchBay
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AsioOut asioRec;
+        private AsioOut asioPlay;
+        private BufferedWaveProvider buffer;
+        private float[] samples = new float[1024 * 1024];
+        private byte[] byteSamples = new byte[1024 * 1024 * 4];
+        private int channels = 0;
+        private bool running;
+
         public MainWindow()
         {
             InitializeComponent();
+            foreach (var device in AsioOut.GetDriverNames())
+            {
+                comboAsioRecordDevices.Items.Add(device);
+                comboAsioPlayDevices.Items.Add(device);
+            }
+
+            if (comboAsioRecordDevices.Items.Count > 0)
+                comboAsioRecordDevices.SelectedIndex = 0;
+
+            if (comboAsioPlayDevices.Items.Count > 0)
+                comboAsioPlayDevices.SelectedIndex = 0;
+
+            Closing += (sender, args) => Stop();
+        }
+
+        private void OnButtonCPClick(object sender, RoutedEventArgs e)
+        {
+            if (sender == buttonPlayCP)
+            {
+                if (asioPlay != null)
+                {
+                    asioPlay.ShowControlPanel();
+                }
+            }
+            else if (sender == buttonRecCP)
+            {
+                if (asioRec != null)
+                {
+                    asioRec.ShowControlPanel();
+                }
+            }
+        }
+
+        private void OnButtonBeginClick(object sender, RoutedEventArgs e)
+        {
+            if (!running)
+            {
+                running = true;
+                asioRec = new AsioOut((string)comboAsioRecordDevices.SelectedItem);
+                asioPlay = new AsioOut((string)comboAsioPlayDevices.SelectedItem);
+
+                stackRecChannels.Children.Clear();
+                for (int i = 0; i < asioRec.DriverInputChannelCount; ++i)
+                {
+                    TextBlock temp = new TextBlock();
+                    temp.Text = asioRec.AsioInputChannelName(i);
+                    stackRecChannels.Children.Add(temp);
+                }
+
+                stackPlayChannels.Children.Clear();
+                for (int i = 0; i < asioPlay.DriverInputChannelCount; ++i)
+                {
+                    TextBlock temp = new TextBlock();
+                    temp.Text = asioPlay.AsioOutputChannelName(i);
+                    stackPlayChannels.Children.Add(temp);
+                }
+
+                //int inputChannels = Math.Min(asioRec.DriverInputChannelCount, 6); // support up to 6 (5.1)
+                //int outputChannels = Math.Min(asioPlay.DriverOutputChannelCount, 6); // support up to 6 (5.1)
+
+                channels = 2;
+                NAudio.Wave.WaveFormat format = new NAudio.Wave.WaveFormat(48000, 32, channels);
+                buffer = new NAudio.Wave.BufferedWaveProvider(format);
+
+                asioRec.InitRecordAndPlayback(null, channels, 48000);
+                asioRec.AudioAvailable += new EventHandler<NAudio.Wave.AsioAudioAvailableEventArgs>(OnAudioAvailable);
+
+                asioPlay.Init(buffer);
+
+                asioRec.Play();
+                asioPlay.Play();
+
+                buttonBegin.Content = "Stop";
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void Stop()
+        {
+            if (running)
+            {
+                asioPlay.Stop();
+                asioPlay.Dispose();
+                asioPlay = null;
+
+                asioRec.Stop();
+                asioRec.Dispose();
+                asioRec = null;
+
+                running = false;
+                buttonBegin.Content = "Begin";
+            }
+        }
+
+        void OnAudioAvailable(object sender, AsioAudioAvailableEventArgs e)
+        {
+            e.GetAsInterleavedSamples(samples);
+            Buffer.BlockCopy(samples, 0, byteSamples, 0, e.SamplesPerBuffer * channels * 4);
+            buffer.AddSamples(byteSamples, 0, e.SamplesPerBuffer * channels * 4);
+            //Trace.WriteLine(buffer.BufferedDuration);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+ASIO Rec and Play
+==========================
+
+This software is a simple program to route the audio from one ASIO interface to another.
+Should be useful to send 5.1/7.1 surround sound from a X-Fi to an HDMI receiver, or allow lossless 5.1 surround sound from a X-Fi Titanium HD.
+
+Still WIP, so no release package yet. Only stereo audio implemented.
